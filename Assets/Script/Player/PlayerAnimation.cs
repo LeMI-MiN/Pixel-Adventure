cd1b48f3888b139d455d9853aeb8ae3db6105c36
미니미니 <psn495268@gmail.com>
2026-07-15 12:01:58 +0900

    0714 업로드(0715복구)

--- Assets/Script/Player/PlayerAnimation.cs
+++ Assets/Script/Player/PlayerAnimation.cs
@@ -2,16 +2,23 @@ using UnityEngine;
 
 public class PlayerAnimation : MonoBehaviour
 {
+    private Animator anim;
+    private Rigidbody2D rb;
 
-
-    void Start()
+    PlayerController controller;
+    
+    private void Awake()
     {
-        
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        controller = GetComponent<PlayerController>();
     }
 
-    void Update()
+    private void Update()
     {
-        
+        anim.SetBool("IsRun", rb.linearVelocity.x != 0);
+        anim.SetBool("IsGround", controller.isGround);
+        anim.SetFloat("YVelocity", rb.linearVelocity.y);
     }
 
 }
--- Assets/Script/Player/PlayerController.cs
+++ Assets/Script/Player/PlayerController.cs
@@ -9,6 +9,14 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private float moveSpeed = 2f;
 
+    // Ground Check
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+    public bool isGround;
+    //
+
+    [SerializeField] private float jumpForce = 3f;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -17,6 +25,27 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         moveInput = Input.GetAxisRaw("Horizontal");
+
+        // Ground Check
+        isGround = Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundLayer);
+        //
+        
+        // Jump
+        if (Input.GetKeyDown(KeyCode.Space) && isGround)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+
+        // Filp
+        if (moveInput > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (moveInput < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
     }
 
     private void FixedUpdate()
